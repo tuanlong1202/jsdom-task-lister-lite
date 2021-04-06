7c2f6ad92e4a873ce2a5d690b97b6a179e68fdbe
tuanlong1202 <tuanlong1202@gmail.com>
2021-04-05 21:48:55 -0400

    Task lister lite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  var taskForm = document.getElementById('create-task-form');
+  var taskDescription = document.getElementById('new-task-description');
+  var ulTask = document.getElementById('tasks');
+  taskForm.addEventListener('submit', function(ev) {
+      ev.preventDefault();
+      ulTask.innerHTML += `<li>` + taskDescription.value + `</li>`;
+      ev.target.reset();
+  })
 });
